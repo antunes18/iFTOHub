d025356fcfa6b1bf880f790b94e8eebb6d91f550
Thiago Antunes <thiagoantunes0101@gmail.com>
2020-11-12 21:08:39 -0300

    Teste

--- php/index.php
+++ php/index.php
@@ -90,13 +90,15 @@ if(!isset($_SESSION['idUser'])){
             <div class="col-md-4">
               <div class="card mb-4 box-shadow">
                 <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text">Hello, World!</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      <a href="https://github.com/iFTOHub/projetos-pdf/raw/main/HelloWorld.pdf">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Download</button>
+                      </a>
+                      <button type="button" class="btn btn-sm btn-outline-secondary" onclick="window.print()">Imprimir</button>
                     </div>
-                    <small class="text-muted">9 mins</small>
+<!--                     <small class="text-muted">9 mins</small> -->
                   </div>
                 </div>
               </div>
