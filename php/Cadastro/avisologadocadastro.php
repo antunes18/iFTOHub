cd07e9be1a8cd067ed2bb00be53d8dda5ddf7108
Thiago Antunes <thiagoantunes0101@gmail.com>
2020-11-14 16:49:22 -0300

    HTML & CSS

--- php/Cadastro/avisologadocadastro.php
+++ php/Cadastro/avisologadocadastro.php
@@ -7,35 +7,30 @@
     <link rel="icon" href="../../logoifhub.png">
     <link href="../../css/bootstrap.min.css" rel="stylesheet">
     <link href="../../css/album.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Spartan&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="icon" href="../../img/logoifhub.png">
-    <style>
-        body {
-            font-family: 'Spartan', sans-serif;
-        }
-  </style>
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
 </head>
 <body>
-    <header>
-      <div class="navbar navbar-dark box-shadow">
-        <div class="container d-flex justify-content-between">
-          <a href="../index.php" class="navbar-brand d-flex align-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-            <strong>NOME_PROJETO</strong>
-          </a>
-        </div>
+  <header>
+    <div class="navbar navbar-dark bg-dark box-shadow">
+      <div class="container d-flex justify-content-between">
+        <a href="../index.php" class="navbar-brand d-flex align-items-center">
+          <img src="../../img/logoifhub.png" alt="Logo iFTOHub" width="40px" height="40px">
+          <strong title="Voltar para a página inicial">iFTOHub</strong>
+        </a>
       </div>
-    </header>
+  </header>
+  <main role="main">
     <div class="jumbotron text-center">
         <h1 class="display-4 animate__heartBeat">Ops...</h1>
-        <p class="lead animate__heartBeat">Verificamos que você está logado em uma conta!</p>
-        <p class="lead animate__heartBeat">Finalize sua seção para poder efetuar um novo cadastro.</p>
+        <p class="lead"><strong>Verificamos que você está logado em uma conta!</strong></p>
+        <p class="lead"><strong>Finalize sua seção para poder efetuar um novo cadastro.</strong></p>
         <hr class="my-4">
     <div class="mt-3">
         <a class="btn btn-lg" href="../index.php" role="button" title="Página inicial"> Voltar para a página inicial </a>
-     </div>
-</div>
+    </div>
+    </div>
+  </main>
 </body>
 </html>
