cd666580359a09ca8844c961b0d4404b2df1653a
Thiago Antunes <thiagoantunes0101@gmail.com>
2020-11-14 16:23:45 -0300

    Allan: Enviar e-mail para o hubifto quando uma solicitação de inserção for requerida

--- php/Projeto/processaprojeto.php
+++ php/Projeto/processaprojeto.php
@@ -1,7 +1,16 @@
 <?php
 session_start();
 require '../conexao.php';
+require_once('../../src/PHPMailer.php');
+require_once('../../src/SMTP.php');
+require_once('../../src/Exception.php');
 global $pdo;
+
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\SMTP;
+        use PHPMailer\PHPMailer\Exception;
+
+        $mailpp = new PHPMailer(true);
 
 if(!isset($_SESSION['idUser'])){
 
@@ -36,6 +45,57 @@ if(isset($_FILES['artigopdf'])){
         $idProjeto = $pdo->lastInsertId();
         $sql = "INSERT INTO iftohub.autorprojeto(idAutor, idProjeto)VALUES($idautor, $idProjeto)";
         $pdo->query($sql);
+
+        if($areaconhecimentop == 'cet'){
+            $areaconhecimentop = 'CIÊNCIAS EXATAS E DA TERRA';
+        }
+        elseif($areaconhecimentop == 'cb'){
+            $areaconhecimentop = 'CIÊNCIAS BIOLÓGICAS';
+        }
+        elseif($areaconhecimentop == 'eng'){
+            $areaconhecimentop = 'ENGENHARIAS';
+        }
+        elseif($areaconhecimentop == 'cs'){
+            $areaconhecimentop = 'CIÊNCIAS DA SAÚDE';
+        }
+        elseif($areaconhecimentop == 'ca'){
+            $areaconhecimentop = 'CIÊNCIAS AGRÁRIAS';
+        }
+        elseif($areaconhecimentop == 'csa'){
+            $areaconhecimentop = 'CIÊNCIAS SOCIAIS APLICADAS';
+        }
+        elseif($areaconhecimentop == 'ch'){
+            $areaconhecimentop = 'CIÊNCIAS HUMANAS';
+        }
+        elseif($areaconhecimentop == 'lla'){
+            $areaconhecimentop = 'LINGUÍSTICA, LETRAS E ARTES';
+        }
+        elseif($areaconhecimentop == 'mul'){
+            $areaconhecimentop = 'MULTIDISCIPLINAR';
+        }
+
+        try{
+        //$mailpp->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mailpp->isSMTP();
+        $mailpp->Host = 'smtp.gmail.com';
+        $mailpp->SMTPAuth = true;
+        $mailpp->Username ='hubifto@gmail.com';
+        $mailpp->Password ='23112019';
+        $mailpp->Port = 587;
+
+        $mailpp->setFrom('hubifto@gmail.com');
+        $mailpp->addAddress('hubifto@gmail.com');
+
+        $mailpp->isHTML(true);
+        $mailpp->Subject = 'Uma nova solicitacao de insercao de projeto foi criada!';
+        $mailpp->Body = "Solicitação de inserção do projeto de título: <b>$titulop</b> <br/><br/>Criada por: <b>$nomeautorp</b> <br/><br/>Área do conhecimento: <b>$areaconhecimentop</b>";
+        $mailpp->AltBody = "Solicitação de inserção do projeto de título: <b>$titulop</b> <br/><br/>Criada por: <b>$nomeautorp</b> <br/><br/>Área do conhecimento: <b>$areaconhecimentop</b>";
+        $mailpp->send();
+
+        header('Location: projeto.php');
+        } catch (Exception $e){
+        echo "Erro ao enviar mensagem: {$mailpp->ErrorInfo}";
+        }
     }
     else{
         $_SESSION['uploaderror'] = true;
