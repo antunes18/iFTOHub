16d0d54c0e333dc5282c27f77f8c74d219594633
Thiago Antunes <thiagoantunes0101@gmail.com>
2020-11-14 10:09:56 -0300

    HTML & CSS: avisoconfirmar.php

--- php/Cadastro/avisoconfirmar.php
+++ php/Cadastro/avisoconfirmar.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confirme seu cadastro</title>
     <link rel="icon" href="../../logoifhub.png">
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../css/album.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="icon" href="../../img/logoifhub.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
 </head>
 <body>
     <?php
@@ -14,9 +19,24 @@
         header('Location: ../index.php');
     }
     ?>
-
-    <h1>Confirme sua conta acessando o link enviado ao seu e-mail para poder aproveitar ao máximo nosso site!</h1>
-    <br>
-    <a href="../index.php">Voltar para a página inicial</a>
+    <header>
+        <div class="navbar navbar-dark bg-dark box-shadow">
+        <div class="container d-flex justify-content-between">
+          <a href="../index.php" class="navbar-brand d-flex align-items-center">
+            <img src="../../img/logoifhub.png" alt="Logo iFTOHub" width="40px" height="40px">
+            <strong title="Voltar para a página inicial">iFTOHub</strong>
+          </a>
+        </div>
+    </header>
+    <main role="main">
+        <div class="jumbotron text-center">
+            <h1 class="display-4 animate__heartBeat">Ops...</h1>
+            <p class="lead"><strong>Confirme sua conta</strong> acessando o link enviado ao seu e-mail para poder aproveitar ao máximo nosso site!</p>
+            <hr class="my-4">
+        <div class="mt-3">
+            <a class="btn btn-lg" href="../index.php" role="button" title="Página inicial">Voltar para a página inicial</a>
+        </div>
+        </div>
+    </main>
 </body>
 </html>
