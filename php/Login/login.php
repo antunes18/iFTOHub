27e5451c5f6d7948cd9fc9ef6a625287ac681f34
Thiago Antunes <thiagoantunes0101@gmail.com>
2020-11-15 20:15:08 -0300

    HTML & CSS

--- php/Login/login.php
+++ php/Login/login.php
@@ -10,12 +10,10 @@ else{
 <!-- saved from url=(0059)https://getbootstrap.com/docs/4.4/examples/floating-labels/ -->
 <html lang="pt-br"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <link href="https://fonts.googleapis.com/css?family=Spartan&display=swap" rel="stylesheet">
     <title>Login</title>
     <link rel="icon" href="../../img/logoifhub.png">
-    <link rel="stylesheet" href="../../css/floating-labels.css">
     <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/floating-labels.css">
     <link rel="stylesheet" href="../../css/style.css">
     <style>
       .bd-placeholder-img {
@@ -31,20 +29,20 @@ else{
           font-size: 3.5rem;
         }
       }
-      body {
-        font-family: 'Spartan', sans-serif;
-      }
-      .notificationfail{
-        padding: 4px;
-        background-color: red;
-        color:black;
+      .error {
+        color:#856404; 
+        background-color:#fff3cd;
+        position:relative;
+        padding:.75rem 1.25rem;
+        margin-bottom:1rem;
+        border-radius:.25rem;
       }
     </style>
   </head>
   <body>
     <form class="form-signin" method="POST" action="processalogin.php">
   <div class="text-center mb-4">
-    <a href="../index.php"><img class="mb-4" src="../../img/logoifhub.png" alt="Aqui vai a logo do site (img)" width="72" height="72"></a>
+    <a href="../index.php"><img class="mb-4" src="../../img/logoifhub.png" alt="Aqui vai a logo do site (img)" width="72" height="72" title="Voltar para a página inicial"></a>
     <!-- <h1 class="h3 mb-3 font-weight-normal">iFTO Hub</h1> -->
     <p>Colocar um texto interessante aqui.</p>
   </div>
@@ -52,9 +50,9 @@ else{
       if(isset($_SESSION['erro'])){
         if($_SESSION['erro'] == true){
       ?>
-      <div class="notificationfail">
-      <p>Erro ao efetuar login!</p>
-      <p>e-mail ou senha incorretos.</p>
+      <div class="alert alert-danger text-center" role="alert">
+        <p>Erro ao efetuar login!</p>
+        <p>e-mail ou senha estão incorretos.</p>
       </div>
       <?php
         }
@@ -64,18 +62,17 @@ else{
   <div class="form-group">
     <span><img src="../../icons/envelope.svg" alt="email icon" height="32" width="32"></span>
     <label for="logemail">Email</label>
-    <input type="email" name="logemail" class="form-control" required autofocus>
+    <input type="email" name="logemail" class="form-control" required autofocus title="Digite seu e-mail utilizado no cadastro">
   </div>
   <div class="form-group">
     <span><img src="../../icons/lock.svg" alt="password icon" height="32" width="32"></span>
     <label for="logsenha">Senha</label>
-    <input type="password" name="logsenha" class="form-control" required>
+    <input type="password" name="logsenha" class="form-control" required title="Digite sua senha utilizada no cadastro">
   </div>
-  <button class="btn btn-lg btn-block" type="submit">Autenticar</button>
+  <button class="btn btn-lg btn-block" type="submit" title="Logar">Autenticar</button>
   <a href="../Cadastro/cadastro.php"  title="Ir para a página de cadastro" class="btn btn-lg btn-block">Primeiro Acesso</a>
   <p class="mt-5 mb-3 text-muted text-center">© 2020</p>
 </form>
-
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.2/dist/jquery.validate.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.2/dist/additional-methods.min.js"></script>
