cf1a4d77edccd0a0623e74334020ad818f6a9e1b
Thiago Antunes <thiagoantunes0101@gmail.com>
2021-01-07 21:12:36 -0300

    Formatar código

--- php/Login/login.php
+++ php/Login/login.php
@@ -1,10 +1,12 @@
 <?php
 session_start();
 
-if(isset($_SESSION['idUser'])){
+if (isset($_SESSION['idUser']))
+{
   header('Location: avisologado.php');
 }
-else{
+else
+{
 ?>
 <!DOCTYPE html>
 <!-- saved from url=(0059)https://getbootstrap.com/docs/4.4/examples/floating-labels/ -->
@@ -45,43 +47,49 @@ else{
     </style>
   </head>
   <body>
-    <form class="form-signin" method="POST" action="processalogin.php">
+    <form class="form-signin" method="POST" action="processalogin.php" autocomplete="off">
   <div class="text-center mb-4">
     <a href="../index.php"><img class="mb-4" src="../../img/logoifhub.png" alt="Aqui vai a logo do site (img)" width="72" height="72" title="Voltar para a página inicial"></a>
     <p class="lead">Bem-vinda(o) de volta!</p>
   </div>
-  <?php
-      if(isset($_SESSION['erro'])){
-        if($_SESSION['erro'] == true){
-      ?>
-      <div class="alert alert-danger text-center" role="alert">
-        <p>Erro ao efetuar login!</p>
-        <p>e-mail e/ou senha estão incorretos.</p>
-      </div>
-      <?php
-      if(empty($_SESSION['contagemerro'])){
-        $_SESSION['contagemerro'] = 0;
-      }
-        if($_SESSION['contagemerro'] < 3){
-          $conta = 3 - $_SESSION['contagemerro'];
-          echo "<p lass='alert alert-info' role='alert'>Você ainda tem $conta tentativas para logar</p>";
-
-          $_SESSION['contagemerro'] = $_SESSION['contagemerro'] + 1;
+    <?php
+    if (isset($_SESSION['erro']))
+    {
+        if ($_SESSION['erro'] == true)
+        {
+    ?>
+          <div class="alert alert-danger text-center" role="alert">
+            <p>Erro ao efetuar login!</p>
+            <p>e-mail e/ou senha estão incorretos.</p>
+          </div>
+          <?php
+            if (empty($_SESSION['contagemerro']))
+            {
+              $_SESSION['contagemerro'] = 0;
+            }
+            if ($_SESSION['contagemerro'] < 3)
+            {
+              $conta = 3 - $_SESSION['contagemerro'];
+              echo "<p lass='alert alert-info' role='alert'>Você ainda tem $conta tentativas para logar</p>";
+              $_SESSION['contagemerro'] = $_SESSION['contagemerro'] + 1;
+            }
+            else
+            {
+              echo "<p class='alert alert-warning' role='alert'>Você atingiu o limite de tentativas de login, tente novamente em 10 minutos./p><br><br>";
+              unset($_SESSION['contagemerro']);
+              $_SESSION['loginbloq'] = true;
+              $_SESSION['horatentada'] = time();
+            }
         }
-        else{
-          echo "<p class='alert alert-warning' role='alert'>Você atingiu o limite de tentativas de login, tente novamente em 10 minutos./p><br><br>";
-          unset($_SESSION['contagemerro']);
-          $_SESSION['loginbloq'] = true;
-          $_SESSION['horatentada'] = time();
-        }
-        }
-      }
-      unset($_SESSION['erro']);
-      if(!isset($_SESSION['loginbloq'])){
-        $_SESSION['loginbloq'] = false;
-      }
-      if($_SESSION['loginbloq'] == false){
-      ?>
+    }
+    unset($_SESSION['erro']);
+    if (!isset($_SESSION['loginbloq']))
+    {
+      $_SESSION['loginbloq'] = false;
+    }
+    if ($_SESSION['loginbloq'] == false)
+    {
+    ?>
   <div class="form-group">
     <span><img src="../../icons/envelope.svg" alt="email icon" height="32" width="32"></span>
     <label for="logemail">Email</label>
@@ -100,57 +108,75 @@ else{
   <a href="../Cadastro/cadastro.php"  title="Ir para a página de cadastro" class="btn btn-lg btn-block">Primeiro Acesso</a>
   <p class="mt-5 mb-3 text-muted text-center">© 2020</p>
   <?php
-      }else{
-        if (isset($_SESSION['loginbloq']) && ($_SESSION['loginbloq'] == true) && isset($_SESSION['horatentada'])){
-         if(time() - $_SESSION['horatentada'] >= 600) {
+  }
+  else
+  {
+      if (isset($_SESSION['loginbloq']) && ($_SESSION['loginbloq'] == true) && isset($_SESSION['horatentada']))
+      {
+          if (time() - $_SESSION['horatentada'] >= 600)
+          {
 
-          // depois de passar 10 minutos:
-          unset($_SESSION['loginbloq']);
-          unset($_SESSION['horatentada']);
-          header("Refresh: 0.5;url=http://localhost/iFTOhub/php/Login/login.php");
+            // depois de passar 10 minutos:
+            unset($_SESSION['loginbloq']);
+            unset($_SESSION['horatentada']);
+            header("Refresh: 0.5;url=http://localhost/iFTOhub/php/Login/login.php");
 
+          }
       }
+      if (isset($_SESSION['loginbloq']))
+      {
+          if ($_SESSION['loginbloq'] == true)
+          {
+              if (isset($_SESSION['horatentada']))
+              {
+                  $contaa = time() - $_SESSION['horatentada'];
+                  if ($contaa >= 0 && $contaa <= 60)
+                  {
+                    $minuto = 10;
+                  }
+                  if ($contaa >= 61 && $contaa <= 120)
+                  {
+                    $minuto = 9;
+                  }
+                  if ($contaa >= 121 && $contaa <= 180)
+                  {
+                    $minuto = 8;
+                  }
+                  if ($contaa >= 181 && $contaa <= 240)
+                  {
+                    $minuto = 7;
+                  }
+                  if ($contaa >= 241 && $contaa <= 300)
+                  {
+                    $minuto = 6;
+                  }
+                  if ($contaa >= 301 && $contaa <= 360)
+                  {
+                    $minuto = 5;
+                  }
+                  if ($contaa >= 361 && $contaa <= 420)
+                  {
+                    $minuto = 4;
+                  }
+                  if ($contaa >= 421 && $contaa <= 480)
+                  {
+                    $minuto = 3;
+                  }
+                  if ($contaa >= 481 && $contaa <= 540)
+                  {
+                    $minuto = 2;
+                  }
+                  if ($contaa >= 541 && $contaa <= 600)
+                  {
+                    $minuto = 1;
+                  }
+                  if (isset($minuto))
+                  {
+                    echo "<p class='alert alert-warning' role='alert'>Ainda faltam <b>$minuto minutos</b> para tentar logar novamente!</p>";
+                  }
+              }
+          }
       }
-      if(isset($_SESSION['loginbloq'])){
-      if($_SESSION['loginbloq'] == true){
-        if(isset($_SESSION['horatentada'])){
-        $contaa = time() - $_SESSION['horatentada'];
-        if($contaa >=0 && $contaa <= 60){
-          $minuto = 10;
-        }
-        if($contaa >=61 && $contaa <= 120){
-          $minuto = 9;
-        }
-        if($contaa >=121 && $contaa <= 180){
-          $minuto = 8;
-        }
-        if($contaa >=181 && $contaa <= 240){
-          $minuto = 7;
-        }
-        if($contaa >=241 && $contaa <= 300){
-          $minuto = 6;
-        }
-        if($contaa >=301 && $contaa <= 360){
-          $minuto = 5;
-        }
-        if($contaa >=361 && $contaa <= 420){
-          $minuto = 4;
-        }
-        if($contaa >=421 && $contaa <= 480){
-          $minuto = 3;
-        }
-        if($contaa >=481 && $contaa <= 540){
-          $minuto = 2;
-        }
-        if($contaa >=541 && $contaa <= 600){
-          $minuto = 1;
-        }
-        if(isset($minuto)){
-        echo "<p class='alert alert-warning' role='alert'>Ainda faltam <b>$minuto minutos</b> para tentar logar novamente!</p>";
-        }
-      }
-      }
-    }
   }
   ?>
 </form>
